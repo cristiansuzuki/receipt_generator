3f960ee9c3b186ca42773819d666d14e2137400d
cristiansuzuki <cristiansuzuki5@hotmail.com>
2023-01-31 21:42:49 -0500

    adding quantity field and set default value

--- receita/forms.py
+++ receita/forms.py
@@ -4,7 +4,7 @@ from .models import *
 class MedicamentoForm(forms.ModelForm):
     class Meta:
         model = Medicamento
-        fields = ('titulo','nome', 'posologia', 'via')
+        fields = ('titulo','nome', 'posologia', 'quantidade', 'via')
         widgets = {
           'titulo': forms.TextInput(
                 attrs={'type': 'text', 'nome': 'titulo', 'placeholder': 'Titulo'}),
@@ -12,6 +12,8 @@ class MedicamentoForm(forms.ModelForm):
                 attrs={'type': 'text', 'nome': 'nome', 'placeholder': 'Nome / Dosagem'}),
             'posologia': forms.TextInput(
                 attrs={'type': 'text', 'nome': 'posologia', 'placeholder': 'Posologia'}),
+            'quantidade': forms.TextInput(
+                attrs={'type': 'text', 'nome': 'quantidade', 'placeholder': 'Quantidade'}),                
             'via': forms.Select(
                 attrs={'nome': 'via'}),
         }
--- receita/models.py
+++ receita/models.py
@@ -12,7 +12,7 @@ class Medicamento(models.Model):
     titulo = models.CharField(max_length=20)
     nome = models.CharField(max_length=150)
     posologia = models.CharField(max_length=150)
-    quantidade = models.CharField(max_length=150, default="------ 01 CAIXA")
+    quantidade = models.CharField(max_length=150, default="_______ 01 CAIXA")
     via = models.ForeignKey(Via, on_delete=models.CASCADE)
 
     def __str__(self):
